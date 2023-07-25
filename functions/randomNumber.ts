96eb0ebf4e6b8973921de9064c339837f0dc12f0
gura105 <shingotanaka9355@gmail.com>
2023-07-26 00:00:21 +0900

    update function

--- functions/randomNumber.ts
+++ functions/randomNumber.ts
@@ -1,4 +1,6 @@
 // Function that return a random number between 0 and 100
-export const randomNumber = () => {
-  return Math.floor(Math.random() * 100);
+export const onRequest: PagesFunction = async () => {
+  const randNum = Math.floor(Math.random() * 100);
+
+  return new Response(randNum.toString());
 };
