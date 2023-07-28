5833c4d2c7564eb2463d3f7c9c3617b80873672a
gura105 <shingotanaka9355@gmail.com>
2023-07-28 19:34:30 +0900

    add posts api

---
+++ functions/post/index.ts
@@ -0,0 +1,40 @@
+// Function that return list of posts
+export const onRequest: PagesFunction = async () => {
+  const posts = await getPosts();
+  return new Response(JSON.stringify(posts));
+};
+
+// Definition of the Post type
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  author: string;
+}
+
+// Data of the List of Post type
+const posts: Post[] = [
+  {
+    id: "1",
+    title: "First Post",
+    content: "This is my first post",
+    author: "John Doe",
+  },
+  {
+    id: "2",
+    title: "Second Post",
+    content: "This is my second post",
+    author: "John Doe",
+  },
+  {
+    id: "3",
+    title: "Third Post",
+    content: "This is my third post",
+    author: "John Doe",
+  },
+];
+
+// Function that return list of posts
+const getPosts = async (): Promise<Post[]> => {
+  return posts;
+};
